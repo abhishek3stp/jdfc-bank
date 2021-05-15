e3ad27bd881dd0f1787d019da8017e068cbcd782
abhishek3stp <abhishek3stp@gmail.com>
2021-05-15 23:26:08 +0530

    error fix

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('transaction/', views.transaction, name='transaction'),
+    path('transaction', views.transaction, name='transaction'),
     path('history/', views.history, name='history'),
     path('', views.home, name='home'),
     path('donate/', views.donate, name='donate'),
